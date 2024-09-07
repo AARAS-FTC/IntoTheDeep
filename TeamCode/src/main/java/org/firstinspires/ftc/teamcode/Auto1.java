a9fe9a439e9d0d8beefd698146f58b6f4030b5fb
aaras <aaras.device@gmail.com>
2024-09-08 09:05:27 +1000

    stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto1.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode;
-
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
-@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = "Auto1")
-public class Auto1 {
-}
