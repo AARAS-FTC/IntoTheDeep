aa9aac3e46e06d52d63397c7f965918d1f59ac4d
bhargav <R1peFruit@users.noreply.github.com>
2024-10-10 11:34:07 +1100

    new test file

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Test.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.robot.Robot;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.SubSystems.GrabberArm;
+import org.firstinspires.ftc.teamcode.SubSystems.GrabberState;
+import org.firstinspires.ftc.teamcode.SubSystems.LinearSlides;
+import org.firstinspires.ftc.teamcode.SubSystems.MechanismState;
+import org.firstinspires.ftc.vision.VisionPortal;
+
+public class Test {
+}
