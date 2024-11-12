ada950cef94c291f531cbb420b176b02490ce0d7
aaras <aaras.device@gmail.com>
2024-11-12 17:14:47 +1100

    added states and modes

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMode.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMode.java
@@ -1,4 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
 public enum RobotMode {
+    SCANNING,
+    COLLECT_SAMPLE,
+    SCORE_SAMPLE_LOW,
+    SCORE_SAMPLE_HIGH,
+    SCORE_SPECIMEN_LOW,
+    SCORE_SPECIMEN_HIGH,
+    CLIMB_LOW,
+    CLIMB_HIGH,
+    DEFAULT
 }
---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/SubsystemStates.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.SubSystems;
+
+public enum SubsystemStates {
+    GRABBER_OPEN,
+    GRABBER_CLOSED,
+    WRIST_POSITION,
+    ARM_SCAN,
+    ARM_POSITION,
+    SLIDE_TOP,
+    SLIDE_POSITION,
+    SLIDE_BOTTOM
+}
